9f68479a9d26260c538a9d5f2a6c61c728658410
Your Name <you@example.com>
2025-03-05 11:48:02 +0300

    added trpc signUp and credentials signIn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 'use client'
 
-import { useSession } from 'next-auth/react'
-
 export default function Home() {
-	const session = useSession()
 	// const SignOut = () => {
 	// 	return <button onClick={() => void signOut()}>Sign Out</button>
 	// }
